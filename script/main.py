9d12157b2da80c4e91927212bf1df69590af2e17
Jackie <jaq.cat@protonmail.ch>
2025-12-25 19:54:38 -0300

    force

--- script/main.py
+++ script/main.py
@@ -1,13 +1,25 @@
 from lib.enable_generator import CharterFilterGenerator
+from lib.events_generator import ColonyEventGenerator
 from templates.colonize_action import ColonizeAction
 import os
 
-for dirpath, dirnames, filenames in os.walk('colonies/enable'):
-    filters = []
-    for file in filenames:
-        g = CharterFilterGenerator(os.path.join(dirpath, file))
-        filters.append(g.get_enabled_filter())
+def enable():
+    for dirpath, dirnames, filenames in os.walk('colonies/enable'):
+        filters = []
+        for file in filenames:
+            g = CharterFilterGenerator(os.path.join(dirpath, file))
+            filters.append(g.get_enabled_filter())
 
-    with open(os.path.join('../in_game/common/generic_actions/htc_colonial_charters_generated.txt'), 'w', encoding='utf-8-sig') as file:
-        action = ColonizeAction.TEMPLATE.format(filters=''.join(filters).rstrip())
-        file.write(action)
+        with open(os.path.join('../in_game/common/generic_actions/htc_colonial_charters_generated.txt'), 'w', encoding='utf-8-sig') as file:
+            action = ColonizeAction.TEMPLATE.format(filters=''.join(filters).rstrip())
+            file.write(action)
+
+def events():
+    for dirpath, dirnames, filenames in os.walk('colonies/events'):
+        filters = []
+        for file in filenames:
+            g = ColonyEventGenerator(os.path.join(dirpath, file))
+            g.write_all('../in_game/events/generated')
+
+enable()
+events()

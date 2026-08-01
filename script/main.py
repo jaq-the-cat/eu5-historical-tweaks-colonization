d485500012ee10adb92963a89609f1e6ca390bfd
Jackie <jaq.cat@protonmail.ch>
2026-08-01 18:46:26 -0300

    Squashed commit of the following:
    commit 2919fb43855375069839bda77e6fc1a53cb46596
    Author: Jackie <jaq.cat@protonmail.ch>
    Date:   Sat Aug 1 18:41:28 2026 -0300
    
        yrsh
    
    commit 1de336860c52274aa4503d31aaeb9edcef83840c
    Author: Jackie <jaq.cat@protonmail.ch>
    Date:   Sat Aug 1 15:10:00 2026 -0300
    
        yes
    
    commit 220b05fe09a37ff5ac18e0e578e5ff365b4bf389
    Author: Jackie <jaq.cat@protonmail.ch>
    Date:   Sat Aug 1 14:39:47 2026 -0300
    
        pre ai

--- script/main.py
+++ script/main.py
@@ -1,10 +1,9 @@
-from lib.enable_generator import CharterFilterGenerator
-from lib.events_generator import ColonyEventGenerator
-from lib.monthly_event_generator import MonthlyPulseGenerator
-from templates.create_colonial_charter.colonize_action import ColonizeAction
 import os
 
 def enable_charters():
+    from lib.enable_generator import CharterFilterGenerator
+    from templates.create_colonial_charter.colonize_action import ColonizeAction
+
     for dirpath, dirnames, filenames in os.walk('colonies/enable'):
         filters = []
         for file in filenames:
@@ -18,6 +17,9 @@ def enable_charters():
                 ColonizeAction.TEMPLATE_BOTTOM)
 
 def charter_events():
+    from lib.events_generator import ColonyEventGenerator
+    from lib.monthly_event_generator import MonthlyPulseGenerator
+
     events = []
     for dirpath, dirnames, filenames in os.walk('colonies/events'):
         filters = []
@@ -27,5 +29,33 @@ def charter_events():
     p = MonthlyPulseGenerator(events)
     p.write_on_action('../in_game/common/on_action/htc_ai_pulse.txt')
 
+def colonial_tags():
+    # OUT = './output/htc_colonial_charter_overrides.txt'
+    # OUT_LOCALIZATION = './output/htc_colonial_countries_l_english.yml'
+    OUT = '../in_game/events/colonization/0_htc_colonial_charter_overrides.txt'
+    OUT_LOCALIZATION = '../main_menu/localization/english/countries/htc_colonial_nations_l_english.yml'
+    from lib.colonial_charter_finished_generator import ColonialCharterFinishedGenerator
+    from templates.events.colonial_charter_finished import ColonialCharterFinishedTemplate
+
+    unique_cns = {}
+
+    for dirpath, dirnames, filenames in os.walk('colonies/tags'):
+        for file in filenames:
+            g = ColonialCharterFinishedGenerator(os.path.join(dirpath, file))
+            unique_cns.update(g.get_unique_colonial_nations())
+    with open(OUT, 'w', encoding='utf-8-sig') as file:
+        file.write(
+            f'namespace = {ColonialCharterFinishedTemplate.NAMESPACE}\n\n' +
+            ColonialCharterFinishedTemplate.EVENT_TEMPLATE.format(
+                COLONIAL_COUNTRY_JOIN_CHECKS = "\n".join(unique_cns["join_checks"]),
+                COLONIAL_COUNTRY_FORM_CHECKS = "\n".join(unique_cns["form_checks"])
+            )
+        )
+    with open(OUT_LOCALIZATION, 'w', encoding='utf-8-sig') as file:
+        file.write(
+            f'l_english:\n' + '\n'.join(unique_cns["localization"])
+        )
+
 enable_charters()
 charter_events()
+colonial_tags()
---
+++ script/templates/events/colonial_charter_finished.py
@@ -0,0 +1,315 @@
+from dataclasses import dataclass
+from templates import TemplateData
+
+@dataclass(frozen=True)
+class ColonialCharterFinishedTemplate(TemplateData):
+    NAMESPACE = 'colonial_charter'
+
+    FORM_TRIGGERS_JOIN = '''
+                            '''
+
+    COLONIAL_COUNTRY_FORM_TEMPLATE = '''
+                    {IF_OR_ELSE_IF} = {{
+                        limit = {{
+                            NOT = {{ country_exists = c:{TAG} }}
+                            {TRIGGERS}
+                        }}
+                        name = {{
+                            name = {TAG}
+                        }}
+                        define_unique_country_tag = {TAG}
+                        change_country_name = {TAG}
+                        change_country_adjective = {TAG}_ADJ
+                        set_variable = {{ name = htc_unique_colony value = yes }}
+                    }}
+'''
+
+    JOIN_TRIGGERS_JOIN = '''
+                '''
+
+    COLONIAL_COUNTRY_JOIN_TEMPLATE = '''
+        {IF_OR_ELSE_IF} = {{
+            limit = {{
+                {TRIGGERS}
+                country_exists = c:{TAG}
+                c:{TAG} ?= {{
+                    is_subject = yes
+                    is_subject_of = scope:actor
+                }}
+            }}
+            c:{TAG} = {{ save_scope_as = unique_colony }}
+        }}
+'''
+
+    EVENT_TEMPLATE = '''colonial_charter.100 = {{
+    hide_portraits = yes
+    type = country_event	
+
+    title = colonial_charter.100.title
+    desc = colonial_charter.100.desc
+    outcome = neutral
+
+    immediate = {{
+        save_scope_as = actor
+        #Select the best location for a capital
+        if = {{
+            limit = {{
+                scope:target = {{
+                    any_location_in_province = {{
+                        is_city = yes
+                        count = 0
+                    }}
+                }}
+            }}
+            scope:target = {{
+                ordered_location_in_province = {{
+                    max = 1
+                    order_by = best_capital_for_colony
+                    
+                    save_scope_as = new_town
+                }}
+            }}       
+        }}
+        
+        #Get a colonial nation vassal neighboring the target province
+        if = {{
+            limit = {{
+                scope:target = {{
+                    any_location_in_province = {{
+                        any_coast_border_location = {{
+                            has_owner = yes
+                            owner = {{
+                                is_subject_of = ROOT
+                                is_subject_type = colonial_nation
+                                NAND = {{ exists = scope:unique_colony this = scope:unique_colony }}
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+            scope:target = {{
+                random_location_in_province = {{
+                    limit = {{
+                        any_coast_border_location = {{
+                            has_owner = yes
+                            owner = {{
+                                is_subject_of = ROOT
+                                is_subject_type = colonial_nation
+                            }}
+                        }}
+                    }}
+                    random_coast_border_location = {{
+                        limit = {{
+                            has_owner = yes
+                            owner = {{
+                                is_subject_of = ROOT
+                                is_subject_type = colonial_nation
+                            }}
+                        }}
+                        owner = {{ save_scope_as = scope_adjacent_area_subject }}
+                    }}
+                }}
+            }}
+        }}
+
+        #Get the most populous colonial nation vassal with capital in the same region
+        ordered_subject = {{
+            limit = {{
+                is_subject_type = colonial_nation
+                capital = {{ region = scope:target.region }}
+                NAND = {{ exists = scope:scope_adjacent_area_subject this = scope:scope_adjacent_area_subject }}
+                NAND = {{ exists = scope:unique_colony this = scope:unique_colony }}
+            }}
+            order_by = total_population
+            max = 1
+            save_scope_as = regional_subject
+        }}
+
+{COLONIAL_COUNTRY_JOIN_CHECKS}
+
+        #Save the Province Capital for the event image
+        scope:target = {{
+            capital = {{ save_scope_as = target_location }}
+        }}
+    }}
+    
+    option = {{ #Form a new Colonial Subject
+        name = colonial_charter.100.a
+        trigger = {{
+            scope:target = {{ is_overseas_for_owner = yes }}
+            or = {{
+                is_ai = no
+                and = {{
+                    is_ai = yes
+                    not = {{ exists = scope:regional_subject }}
+                    not = {{ exists = scope:scope_adjacent_area_subject }}
+                    not = {{ exists = scope:unique_colony }}
+                }}
+            }}
+        }}
+        
+        if = {{
+            limit = {{
+                exists = scope:new_town
+            }}
+            scope:new_town = {{
+                change_location_rank_effect = {{ location_rank = location_rank:town }}
+            }}
+        }}
+        
+        scope:target = {{ 
+            create_location_country_from_province = {{
+                subject_type = subject_type:colonial_nation
+            
+                hidden_effect = {{
+                    setup_colonial_nation = yes
+
+{COLONIAL_COUNTRY_FORM_CHECKS}
+
+                }}
+            }}
+        }}
+        
+        ai_will_select = {{
+            value = 50
+        }}
+    }}
+    
+    option = {{
+        name = htc_unique_colonies.a
+
+        trigger = {{
+            exists = scope:unique_colony
+        }}
+        
+        scope:target = {{
+            change_province_owner = scope:unique_colony
+        }}
+
+        ai_will_select = {{
+            value = 9999
+        }}
+    }}
+
+    option = {{ # Join it to scope_adjacent_area_subject
+        name = colonial_charter.100.b
+
+        trigger = {{
+            exists = scope:scope_adjacent_area_subject
+            not = {{ exists = scope:unique_colony }}
+        }}
+
+        if = {{
+            limit = {{
+                exists = scope:new_town
+            }}
+            scope:new_town = {{
+                change_location_rank_effect = {{ location_rank = location_rank:town }}
+                cc_setup_new_town = yes
+            }}
+        }}
+
+        scope:target = {{
+            change_province_owner = scope:scope_adjacent_area_subject
+        }}
+
+        ai_will_select = {{
+            value = 1000
+        }}
+    }}
+
+    option = {{ # Join it to regional_subject
+        name = colonial_charter.100.f
+
+        trigger = {{
+            exists = scope:regional_subject
+            not = {{ exists = scope:unique_colony }}
+        }}
+
+        if = {{
+            limit = {{
+                exists = scope:new_town
+            }}
+            scope:new_town = {{
+                change_location_rank_effect = {{ location_rank = location_rank:town }}
+                cc_setup_new_town = yes
+            }}
+        }}
+
+        scope:target = {{
+            change_province_owner = scope:regional_subject
+        }}
+
+        ai_will_select = {{
+            value = 2000
+        }}
+    }}
+
+    option = {{ #Keep it as part of the Metropolis
+        name = colonial_charter.100.d
+        
+        trigger = {{
+            or = {{
+                is_ai = no
+                scope:target = {{ is_overseas_for_owner = no }}
+            }}
+        }}
+
+        scope:target = {{ 
+            every_location_in_province = {{
+                limit = {{ integration_level = colonized }}
+                change_integration_level = integrated
+            }}
+        }}
+
+        ai_chance = {{
+            value = 100
+        }}
+    }}
+
+    option = {{ #Play as the Colony
+        name = colonial_charter.100.e
+        high_risk_option = yes
+        trigger = {{
+            is_human = yes
+            scope:target = {{ is_overseas_for_owner = yes }}
+        }}
+        if = {{
+            limit = {{
+                exists = scope:new_town
+            }}
+            scope:new_town = {{
+                change_location_rank_effect = {{ location_rank = location_rank:town }}
+            }}
+        }}
+        scope:target = {{ 
+            create_location_country_from_province = {{
+                subject_type = subject_type:colonial_nation
+                hidden_effect = {{
+                    setup_colonial_nation = yes
+                }}
+                save_scope_as = target_country
+            }}
+        }}
+        custom_tooltip = {{
+            text = colonial_charter.100.e.tt
+            change_player = scope:target_country
+        }}
+
+        # needed when ai and human is playing together
+        ai_chance = {{
+            value = -1000
+        }}
+    }}
+
+    after = {{
+        trigger_event_silently = treaty_of_tordesillas.1
+    }}
+}}
+'''
+
+    
+    COLONIAL_COUNTRY_LOCALIZATION_TEMPLATE = '''
+  {TAG}: "{NAME}"
+  {TAG}_ADJ: "{ADJECTIVE}"
+'''
